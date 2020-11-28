d8137b59adcb9c628b4a133548f3ea74f06d5265
Sander Aasmaa <sander.aasmaa@gmail.com>
2020-11-28 14:49:04 +0200

    Seeders controller

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Service;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            ServiceSeeder::class,
+            ServicePassSeeder::class,
+            CustomerSeeder::class,
+        ]);
     }
 }
